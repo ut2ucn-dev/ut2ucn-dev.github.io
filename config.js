9ca98d6fca03b3f185ea7efa29d2e1263637413a
ut2ucn-dev <ut2ucn@uaro.org.ua>
2026-09-06 18:46:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // Цей файл можна редагувати прямо на GitHub, без будь-яких збірок.
 
 window.SUPABASE_URL = "https://smfwlbbjcfezmayvcofv.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_fZnHIREvMc1ewsRQ7WWaLw_C2SjZCmP";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtZndsYmJqY2Zlem1heXZjb2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2OTg5ODgsImV4cCI6MjEwNDI3NDk4OH0.JqdFphXJFwxwe0lAdRwApjz15fAqP9Ezgx5WXQ-lx9U";
